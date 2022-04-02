142519a9f5686f3df575d0fbdd1c6c8615887129
Ravi <54009887+RaviMahey@users.noreply.github.com>
2022-04-02 13:15:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-var allData= JSON.parse(localStorage.getItem('list') );
-document.body.style.zoom="100%";
+var allData = JSON.parse(localStorage.getItem('list'));
+document.body.style.zoom = "100%";
 console.log(allData);
-var temp= "row rounded-0 p-1  alert alert-success";
-var zerOneTwo=-1;
-function displayList(){
-	var inHtml="";
-	let single = document.getElementById('LIST');
-	allData= JSON.parse(localStorage.getItem('list'));
-	for (var i = allData.length - 1; i >= 0; i--) {		
-		let cell= `
+var temp = "row rounded-0 p-1  alert alert-success";
+var zerOneTwo = -1;
+
+function displayList() {
+    var inHtml = "";
+    let single = document.getElementById('LIST');
+    allData = JSON.parse(localStorage.getItem('list'));
+    for (var i = allData.length - 1; i >= 0; i--) {
+        let cell = `
     <div class="container-fluid w-100 border-bottom d-flex align-items-center  bg-white rounded id=${i}">
     		<div class="col overflow-hidden">${allData[i]}</div>
     		<div class="col-5 btn-group btn-group-sm" >
@@ -18,96 +18,90 @@ function displayList(){
     			
     		</div>
     	</div><br>`;
-		inHtml += cell;
-	}
-	single.innerHTML=inHtml;
-	return;
-
-}
-function showAlert(){
-	if(zerOneTwo==0){
-		// document.getElementById('taskAdded').innerHTML="Task is Added";
-		document.getElementById('taskAdded').style.visibility="hidden";
-	}
-	else if(zerOneTwo==1){
-		// document.getElementById('taskAdded').innerHTML="Task is Done";
-		document.getElementById('taskAdded').style.visibility="hidden";
-	}
-	else if(zerOneTwo==2){
-		// document.getElementById('taskdd').innerHTML="Task is deleted";
-
-		document.getElementById('taskAdded').style.visibility="hidden";
-		document.getElementById('taskAdded').className=temp;
-
-	}
-}
-function deleteFromList(indexOfList,doneDel){
-
-	allData.splice(indexOfList,1);
-	// alert(allData);
-	localStorage.setItem('list' ,JSON.stringify(allData));
-	displayList();	
-	if(doneDel==0){
-
-		zerOneTwo= 1;
-		document.getElementById('taskAdded').className = temp;
-		document.getElementById('taskAdded').innerHTML="Task is Done";
-		document.getElementById('taskAdded').style.visibility="visible";
-		let t1=setTimeout(showAlert,2000);
-
-
-	}
-	else{	
-
-			
-		document.getElementById('taskAdded').innerHTML="Task is Deleted";
-		document.getElementById('taskAdded').className="row rounded-0 p-1  alert alert-danger";
-
-		document.getElementById('taskAdded').style.visibility="visible";
-		zerOneTwo=2;
-		let t2=setTimeout(showAlert,2000);
-		
-
-		
-	}
-
-	
-	return;
+        inHtml += cell;
+    }
+    single.innerHTML = inHtml;
+    return;
 
 }
 
-function  addToList(){
-	 event.preventDefault();
-	if(localStorage.getItem('list')==null){
-		localStorage.setItem('list','[]');
-		allData= [];
-	}
-	var inputText= document.getElementById("inputText").value;
-	if(inputText==""){
-		alert("field is empty");
-		return;
-	}
-	allData.push(inputText);
+function showAlert() {
+    if (zerOneTwo == 0) {
+        
+        document.getElementById('taskAdded').style.visibility = "hidden";
+    } else if (zerOneTwo == 1) {
 
-	// alert(allData);
-	localStorage.setItem('list',JSON.stringify(allData));
-	document.getElementById("inputText").value="";	
-	document.getElementById('taskAdded').className = temp;
+        document.getElementById('taskAdded').style.visibility = "hidden";
+    } else if (zerOneTwo == 2) {     
 
-	document.getElementById('taskAdded').innerHTML="Task is Added";
-	document.getElementById("taskAdded").style.visibility="visible";
-	zerOneTwo=0;
-	let timeOut=setTimeout(showAlert,2000);
+        document.getElementById('taskAdded').style.visibility = "hidden";
+        document.getElementById('taskAdded').className = temp;
 
-	displayList();
+    }
+}
 
+function deleteFromList(indexOfList, doneDel) {
+
+    allData.splice(indexOfList, 1);
+    localStorage.setItem('list', JSON.stringify(allData));
+    displayList();
+    if (doneDel == 0) {
+
+        zerOneTwo = 1;
+        document.getElementById('taskAdded').className = temp;
+
+        document.getElementById('taskAdded').innerHTML = "Task is Done";
+        document.getElementById('taskAdded').style.visibility = "visible";
+
+        let t1 = setTimeout(showAlert, 2000);
+
+
+    } else {
+
+
+        document.getElementById('taskAdded').innerHTML = "Task is Deleted";
+        document.getElementById('taskAdded').className = "row rounded-0 p-1  alert alert-danger";
+
+        document.getElementById('taskAdded').style.visibility = "visible";
+        zerOneTwo = 2;
+        let t2 = setTimeout(showAlert, 2000);
+    }
+    return;
 
 }
-function clicked(){
-	document.getElementById('taskAdded').style.display="none";
-	document.getElementById('taskdd').style.display="none";
 
+function addToList() {
+    event.preventDefault();
+    if (localStorage.getItem('list') == null) {
+        localStorage.setItem('list', '[]');
+        allData = [];
+    }
+    var inputText = document.getElementById("inputText").value;
+    if (inputText == "") {
+        alert("field is empty");
+        return;
+    }
+    allData.push(inputText);
+
+    // alert(allData);
+    localStorage.setItem('list', JSON.stringify(allData));
+    document.getElementById("inputText").value = "";
+    document.getElementById('taskAdded').className = temp;
+
+    document.getElementById('taskAdded').innerHTML = "task is Added";
+    document.getElementById("taskAdded").style.visibility = "visible";
+    zerOneTwo = 0;
+    let timeOut = setTimeout(showAlert, 2000);
+
+    displayList();
 
 
 }
 
+function clicked() {
+    document.getElementById('taskAdded').style.display = "none";
+    document.getElementById('taskdd').style.display = "none";
+
+
+
+}
