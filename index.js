5287da059e336f82d7d1f5cf4de3ab5bd81e5ba2
Ravi <54009887+RaviMahey@users.noreply.github.com>
2022-04-02 11:37:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 var allData= JSON.parse(localStorage.getItem('list') );
 document.body.style.zoom="100%";
 console.log(allData);
+let temp= "row rounded-0 p-1  alert alert-success";
+var zerOneTwo=-1;
 function displayList(){
 	var inHtml="";
 	let single = document.getElementById('LIST');
 	allData= JSON.parse(localStorage.getItem('list'));
 	for (var i = allData.length - 1; i >= 0; i--) {		
 		let cell= `
-    <div class="w-100 border-bottom d-flex align-items-center  bg-white rounded id=${i}">
+    <div class="container-fluid w-100 border-bottom d-flex align-items-center  bg-white rounded id=${i}">
     		<div class="col overflow-hidden">${allData[i]}</div>
     		<div class="col-5 btn-group btn-group-sm" >
-				<button type="button" class="btn btn-outline-success my-1" onclick="deleteFromList()">done</button>
-  				<button type="button" class="btn btn-danger my-1" onclick="deleteFromList()">delete</button>
+				<button type="button" class="btn btn-outline-success my-1" onclick="deleteFromList(${i},0)">done</button>
+  				<button type="button" class="btn btn-danger my-1" onclick="deleteFromList(${i},1)">delete</button>
     			
     		</div>
     	</div><br>`;
@@ -22,14 +24,57 @@ function displayList(){
 	return;
 
 }
-function deleteFromList(indexOfList){
+function showAlert(){
+	if(zerOneTwo==0){
+		// document.getElementById('taskAdded').innerHTML="Task is Added";
+		document.getElementById('taskAdded').style.visibility="hidden";
+	}
+	else if(zerOneTwo==1){
+		// document.getElementById('taskAdded').innerHTML="Task is Done";
+		document.getElementById('taskAdded').style.visibility="hidden";
+	}
+	else if(zerOneTwo==2){
+		// document.getElementById('taskdd').innerHTML="Task is deleted";
+
+		document.getElementById('taskAdded').style.visibility="hidden";
+		document.getElementById('taskAdded').className=temp;
+
+	}
+}
+function deleteFromList(indexOfList,doneDel){
+
 	allData.splice(indexOfList,1);
 	// alert(allData);
 	localStorage.setItem('list' ,JSON.stringify(allData));
-	displayList();
+	displayList();	
+	if(doneDel==0){
+
+		zerOneTwo= 1;
+		document.getElementById('taskAdded').innerHTML="Task is Done";
+		document.getElementById('taskAdded').style.visibility="visible";
+		let t1=setTimeout(showAlert,2000);
+
+
+	}
+	else{	
+
+			
+		document.getElementById('taskAdded').innerHTML="Task is Deleted";
+		document.getElementById('taskAdded').className="row rounded-0 p-1  alert alert-danger";
+
+		document.getElementById('taskAdded').style.visibility="visible";
+		zerOneTwo=2;
+		let t2=setTimeout(showAlert,2000);
+		
+
+		
+	}
+
+	
 	return;
 
 }
+
 function  addToList(){
 	 event.preventDefault();
 	if(localStorage.getItem('list')==null){
@@ -45,7 +90,21 @@ function  addToList(){
 
 	// alert(allData);
 	localStorage.setItem('list',JSON.stringify(allData));
-	document.getElementById("inputText").value="";
+	document.getElementById("inputText").value="";	
+	document.getElementById('taskAdded').innerHTML="task is Added";
+	document.getElementById("taskAdded").style.visibility="visible";
+	zerOneTwo=0;
+	let timeOut=setTimeout(showAlert,2000);
+
 	displayList();
 
+
 }
+function clicked(){
+	document.getElementById('taskAdded').style.display="none";
+	document.getElementById('taskdd').style.display="none";
+
+
+
+}
+
